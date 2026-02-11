480accd75fcb5fd8e5a251ca66e762efc2102a82
inmortalmode17-cpu <inmortalmode17@gmail.com>
2026-02-10 20:14:59 -0500

    Create patterns.js

---
+++ const patterns = require("./patterns.js
@@ -0,0 +1,30 @@
+module.exports = {
+  lenguaje: {
+    palabras: [
+      "libertad financiera",
+      "ingresos pasivos",
+      "método secreto",
+      "sin experiencia",
+      "desde tu celular"
+    ],
+    peso: 5
+  },
+
+  promesas: {
+    palabras: [
+      "resultados garantizados",
+      "dinero asegurado",
+      "sin riesgo"
+    ],
+    peso: 15
+  },
+
+  urgencia: {
+    palabras: [
+      "últimos cupos",
+      "solo hoy",
+      "oferta limitada"
+    ],
+    peso: 10
+  }
+};
